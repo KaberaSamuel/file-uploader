f0f73bddc25210b80f419468992d92f542582329
KaberaSamuel <kaberanshutis@gmail.com>
2025-04-14 00:08:46 +0200

    basic layout for home route and function to create folders tree

--- db/queries.js
+++ db/queries.js
@@ -11,6 +11,24 @@ async function createTableUsers() {
       `);
 }
 
+async function createTableFolders() {
+  await pool.query(`
+    CREATE TABLE folders (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(255),
+        parent_id INT,
+        user_id INT
+    );
+  `);
+}
+
+async function insertIntoFolders(name, parent_id, user_id) {
+  await pool.query(
+    "INSERT INTO folders (name, parent_id, user_id) VALUES ($1, $2, $3)",
+    [name, parent_id, user_id]
+  );
+}
+
 async function insertIntoUsers(username, email, password) {
   await pool.query(
     "INSERT INTO users (username, email,password) VALUES ($1, $2, $3)",
@@ -40,4 +58,37 @@ async function getUserById(id) {
   return rows[0];
 }
 
-export { insertIntoUsers, getUserByEmail, getUserByPassword, getUserById };
+async function getTopFoldersByUser(userId) {
+  const { rows } = await pool.query(
+    `SELECT * FROM folders WHERE (user_id='${userId}') AND (parent_id=0);`
+  );
+
+  return rows;
+}
+
+async function getFoldersByUser(userId) {
+  const { rows } = await pool.query(`SELECT * FROM folders;`);
+
+  return rows;
+}
+
+async function getFolderchildren(userId, folderId) {
+  const { rows } = await pool.query(
+    `SELECT * FROM folders WHERE (user_id=${userId}) AND (parent_id=${folderId});`
+  );
+
+  return rows;
+}
+
+insertIntoFolders("missy", 27, 35);
+
+export {
+  insertIntoUsers,
+  insertIntoFolders,
+  getUserByEmail,
+  getUserByPassword,
+  getUserById,
+  getTopFoldersByUser,
+  getFoldersByUser,
+  getFolderchildren,
+};
